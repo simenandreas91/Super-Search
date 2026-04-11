38099ca4c7517696b676e165fd961eca3a9cb6d6
simenandreas91 <simenandreas91@hotmail.no>
2026-04-11 15:59:01 +0200

    yes

--- dev316614/x_1122545_super_0/sp_widget/Supersearchwidget/script.js
+++ dev316614/x_1122545_super_0/sp_widget/Supersearchwidget/script.js
@@ -1,9 +1,17 @@
 (function() {
+  var currentUser = gs.getUser();
+  var firstName = '';
+
+  if (currentUser && currentUser.getFirstName) {
+    firstName = currentUser.getFirstName() || '';
+  }
+
   data.config = {
     resultsPageId: options.results_page_id || 'search_results',
-    inputPlaceholder: options.input_placeholder || 'Sok i kunnskapsbasen',
+    inputPlaceholder: options.input_placeholder || 'Finn artikler, bestillinger og record producers fra ett sok.',
     buttonLabel: options.button_label || 'Sok'
   };
 
+  data.greetingName = firstName;
   data.initialQuery = $sp.getParameter('q') || '';
 })();
