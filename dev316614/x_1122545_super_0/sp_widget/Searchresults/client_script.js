00cc8293b095e2d5bf2e996e0e4e2dc2ee07e301
simenandreas91 <simenandreas91@hotmail.no>
2026-04-12 15:44:48 +0200

    yes

--- dev316614/x_1122545_super_0/sp_widget/Searchresults/client_script.js
+++ dev316614/x_1122545_super_0/sp_widget/Searchresults/client_script.js
@@ -3,23 +3,54 @@ api.controller = function($location) {
 
   c.search = c.data.search;
   c.isLoading = false;
+  c.normalizeQuery = function(value) {
+    return (value || '').replace(/\s+/g, ' ').replace(/^\s+|\s+$/g, '');
+  };
 
-  c.goToPage = function(page) {
-    if (c.isLoading || !c.search || page < 1 || page > c.search.totalPages || page === c.search.page) {
+  c.executeSearch = function(request) {
+    var nextQuery = c.search ? c.search.query : '';
+    var nextPage = c.search ? c.search.page : 1;
+    var nextFilter = c.search ? c.search.activeFilter : 'all';
+
+    request = request || {};
+
+    if (typeof request.query !== 'undefined') {
+      nextQuery = c.normalizeQuery(request.query);
+    }
+
+    if (typeof request.page !== 'undefined') {
+      nextPage = request.page;
+    }
+
+    if (typeof request.resultFilter !== 'undefined') {
+      nextFilter = request.resultFilter;
+    }
+
+    if (!nextQuery) {
       return;
     }
 
     c.isLoading = true;
     c.server.get({
-      query: c.search.query,
-      page: page,
-      resultFilter: c.search.activeFilter
+      query: nextQuery,
+      page: nextPage,
+      resultFilter: nextFilter
     }).then(function(response) {
       c.search = response.data.search;
       c.updateUrl();
       c.isLoading = false;
     }, function() {
       c.isLoading = false;
+    });
+  };
+
+  c.goToPage = function(page) {
+    if (c.isLoading || !c.search || page < 1 || page > c.search.totalPages || page === c.search.page) {
+      return;
+    }
+
+    c.executeSearch({
+      page: page
     });
   };
 
@@ -32,22 +63,36 @@ api.controller = function($location) {
       return;
     }
 
-    c.isLoading = true;
-    c.server.get({
-      query: c.search.query,
+    c.executeSearch({
       page: 1,
       resultFilter: filterId
-    }).then(function(response) {
-      c.search = response.data.search;
-      c.updateUrl();
-      c.isLoading = false;
-    }, function() {
-      c.isLoading = false;
     });
   };
 
   c.isActiveFilter = function(filterId) {
     return c.search && c.search.activeFilter === filterId;
+  };
+
+  c.hasZeroResultState = function() {
+    return c.search && c.search.zeroResultState;
+  };
+
+  c.runSuggestedQuery = function(query) {
+    var normalizedQuery = c.normalizeQuery(query);
+
+    if (c.isLoading || !normalizedQuery) {
+      return;
+    }
+
+    if (c.search && c.search.query === normalizedQuery && c.search.activeFilter === 'all' && c.search.page === 1) {
+      return;
+    }
+
+    c.executeSearch({
+      query: normalizedQuery,
+      page: 1,
+      resultFilter: 'all'
+    });
   };
 
   c.getStartIndex = function() {
